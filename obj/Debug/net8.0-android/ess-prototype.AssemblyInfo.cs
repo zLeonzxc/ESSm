12f4085af272425a8d1c3ddb9fc5fe16949f00b9
leon <leon.karjunn@gmail.com>
2024-10-22 14:39:06 +0800

    Edited icons + page format

--- obj/Debug/net8.0-android/ess-prototype.AssemblyInfo.cs
+++ obj/Debug/net8.0-android/ess-prototype.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ess-prototype")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+61f89484326ee65c2645e0a2f4b38684bd530649")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+c0f0b60ff06087a2efe4ea6316cf82ffbf8cb972")]
 [assembly: System.Reflection.AssemblyProductAttribute("ess-prototype")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ess-prototype")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
