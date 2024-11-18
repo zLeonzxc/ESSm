fb02172ac1175f12160ab3387e1c924225314e78
zLeonzxc <leon.karjunn@gmail.com>
2024-11-18 14:42:04 +0800

    Reworked main page

--- Views/MainPage.xaml.cs
+++ Views/MainPage.xaml.cs
@@ -7,5 +7,40 @@
             InitializeComponent();
         }
 
+        private async void OnSearchEmployeeClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                await Shell.Current.Navigation.PushAsync(new EmployeeDetailsPage());
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Navigation error: {ex.Message}");
+            }
+        }
+
+        private async void OnLeaveListClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                await Shell.Current.Navigation.PushAsync(new EmployeeLeaveList());
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Navigation error: {ex.Message}");
+            }
+        }
+
+        private async void OnApprovalClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                await Shell.Current.Navigation.PushAsync(new PendingApprovalPage());
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Navigation error: {ex.Message}");
+            }
+        }
     }
 }
